1ce5d049cfd5d23dec857916281bddb83ce2adfc
Andrey Polyskalov <contact@polyskalov.com>
2013-07-25 09:10:37 +0600

    Изменение формы и кода
    — Функции перенесены в хелпер (класс модуля)
    — Выбор абонемента
    — Удалён ненужный (вообще-то, не совсем) div, инлайновые стили
    перенесены в отдельный стилевой блок
    — внеше немного изменена форма (оступы, ширина элементов)

--- public/mod_books_extension.php
+++ public/mod_books_extension.php
@@ -1,77 +1,82 @@
-<style>
-#mod_book_ext_form_user table {
-	width:100%;
-
-}
-
-#mod_book_ext_form_user table input {
-	width:70%;
-}
-
-#mod_book_ext_form_requests ul li {
-	list-style: none;
-	list-style-type: none;
-
-	padding-bottom: 1em;
-}
-</style>
-
-
-<?php
-
-function validateEmail($email) {
-  if(preg_match('/^([\w\-\.])+@([\w\-\.])+\.([a-z0-9])+$/i', $email) == 0)  return FALSE; else  return TRUE;
-}
-
-require 'mod_books_extension_class.php';
-
-$bookExtModule = new Book_extension;
-
-switch ( $_GET['book_ext_action'] ) {
+<form id="mod_book_ext_form" method="post" action="?book_ext_action=request">
 	
-	case 'request':
-		$name   = $_POST['name'];
-		$email  = $_POST['email'];
-		$book   = $_POST['book'];
-
-		if (! validateEmail($email) ) {
-			echo '<br><p class="tip">Ошибка: Введён неправильный email!</p>';
-			break;
+	<style>
+		#mod_book_ext_form {
+			margin:          20px 17px !important;
 		}
+		#mod_book_ext_form table {
+			border-collapse: collapse;
+			border-spacing:  0;
+			margin:          10px 0;
+			width:           100%;
+		}
+		#mod_book_ext_form table td, #mod_book_ext_form table tr {
+			vertical-align:  top;
+		}
+
+		#mod_book_ext_form table input, #mod_book_ext_form table select {
+			width:           100%;
+		}
+	</style>
+
+	<?php
+
+	require 'mod_books_extension_class.php';
+	$bookExtModule = new Book_extension;
+
+
+	$abonements = array('Младший', 'Юношеский', 'Старший');
+
+
+	switch ( $_GET['book_ext_action'] ) {
 		
-		try {
-			$requestStatus = $bookExtModule-> addRequest($name, $email, $book);
+		case 'request':
+			$name       = $_POST['name'];
+			$email      = $_POST['email'];
+			$abonement  = $_POST['abonement'];
+			$book       = $_POST['book'];
 
-			if ($requestStatus) {
-				echo '<br><p class="tip">Спасибо, Ваша заявка на продление принята в обработку.<p>';
-			} else {
-				echo '<br><p class="tip">Извините, не удалось принять заявку в обработку.<p>';
+			if (! validateEmail($email) ) {
+				echo '<p class="tip">Ошибка: Введён неправильный email!</p>';
+				break;
 			}
+			
+			try {
+				if ( $bookExtModule-> addRequest($name, $email, $abonement, $book) ) {
+					echo '<p class="tip">Спасибо, Ваша заявка на продление принята в обработку.</p>';
+				} else {
+					echo '<p class="tip">Извините, не удалось принять заявку в обработку.</p>';
+				}
 
-		} catch (Book_extension_exception $e) {
-			echo '<p class="tip">Ошибка: '.$e-> getMessage().'</p>';
-		}
-	break;
-	
-	default:
-		# code...
-	break;
-}
-?>
-<div style="margin: 20px 17px">
-	<form id="mod_book_ext_form_user" method="post" action="?book_ext_action=request">
-	
-		<table width="100%">
-			<tr>
-				<td style="width:30%;"><label for="bookext_form_name">Введите Ваши Ф.И.О:</label></td><td><input id="bookext_form_name" name="name" required></td>
-			</tr>
-			<tr>
-				<td><label for="bookext_form_email">Введите Ваш email:</label></td><td><input id="bookext_form_email" name="email" type="email" required><br><span style="color:#555;font:13px arial">(мы уведомим Вас о продлении)</span></td>
-			</tr>
-			<tr>
-				<td><label for="bookext_form_book">И название книги:</label></td><td><input id="bookext_form_book" name="book" required></td>
-			</tr>
-		</table>
-		<p><button type="submit">Запросить продление</button></p>
-	</form>
-</div>
+			} catch (Book_extension_exception $e) {
+				echo '<p class="tip">Ошибка: '.$e-> getMessage().'</p>';
+			}
+		break;
+	}
+	?>
+
+	<table>
+		<tr>
+			<td style="width:30%;"><label for="bookext_form_name">Введите Ваши Ф.И.О:</label></td><td><input id="bookext_form_name" name="name" required></td>
+		</tr>
+		<tr>
+			<td><label for="bookext_form_abonement">Выберите абонемент:</label></td>
+			<td>
+				<select id="bookext_form_abonement" name="abonement" required>
+					<?php
+					foreach ($abonements as $id => $name) {
+						echo "<option value=\"$id\">$name</option>";
+					}
+					?>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td><label for="bookext_form_email">Введите email:</label></td><td><input id="bookext_form_email" name="email" type="email" required><br><span style="color:#555;font:13px arial">(мы уведомим Вас о продлении)</span></td>
+		</tr>
+		<tr>
+			<td><label for="bookext_form_book">И название книги:</label></td><td><input id="bookext_form_book" name="book" required></td>
+		</tr>
+	</table>
+	<p><button type="submit" class="button">Запросить продление</button></p>
+</form>
