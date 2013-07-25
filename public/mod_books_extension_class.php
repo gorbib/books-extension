5c733251f145c551342841fcf08d6696f529158a
Andrey Polyskalov <contact@polyskalov.com>
2013-07-25 08:57:48 +0600

    Улучшение кода классов модуля
    Удалено большое количество неиспользуемого кода, добавлено больше
    документации,
    Возможность использования префикса базы данных, обработка дополнительны�
    полей формы,
    функции перенесены сюда же

--- public/mod_books_extension_class.php
+++ public/mod_books_extension_class.php
@@ -1,151 +1,56 @@
 <?php
 
 class Book_extension  extends JObject {
+	/**
+	 * Объект для взаимодействия с базой данных
+	 * @var Object
+	 */
 	protected $_db;
 
-	// Уведомлять ли по электропочте
-	private static $notificateByEmail = true;
+	/**
+	 * Имя таблицы с данными модуля в базе данных
+	 * @var string
+	 */
+	private $_db_tablename = 'booksextension';
 
-	// Если пользователь — редактор, (переменная = true)
-	public static $isEditor;
-
-	// Группа администраторов
-	public static $allowedUserGroup = 9;
-
-
+	/**
+	 * Инициализация модуля
+	 */
 	function __construct() {
-		jimport('joomla.access.access');
-
-		$user = JFactory::getUser();
-
-		$userGroup = JAccess::getGroupsByUser($user->id);
-
-		$this-> isEditor = false;
-		//($_SERVER['REMOTE_ADDR'] == '192.168.0.107');
-
 		$this->_db = JFactory::getDbo();
 	}
 
-	// Отправка уведомления на почту
-	public function notificate($requestID, $status, $reason = '') {
-		$headers  = "From: $mailFrom\r\n"; 
-		$headers .= "Content-type: text/html;charset=utf-8\r\n";
+	/**
+	 * Добавление заявки в базу данных
+	 * @param string  $name      Имя пользователя
+	 * @param string  $email     Электропочта пользователя
+	 * @param boolean $abonement Абонемент
+	 * @param string  $book      Название книги
+	 */
+	public function addRequest($name, $email, $abonement, $book) {
+		$name       = addslashes( htmlspecialchars($name) );
+		$email      = addslashes( htmlspecialchars($email) );
+		$abonement  = intval($abonement);
+		$book       = addslashes( htmlspecialchars($book) );
 
-		$data = $this-> getRequest($requestID);
-
-		if ( empty($data) ) return false;
-
-		$email = $data['email'];
-		$name  = $data['name'];
-
-
-		if ( empty($email) ) return false;
-
-		if ( empty($name) ) $name = 'Дорогой читатель';
-
-		// Сообщения, отправляемые пользователю
-		$Messages = array(
-			// Продлено
-			'accepted' => array(
-				'subject' => 'Книга продлена',
-				'text'    => "<p>Здравствуйте, $name. </p><br><p>Мы обработали Вашу заявку и продлили книгу <i>{$data['book']}</i>. Приятного чтения.</p> <br> -- <br> С уважением, качканарская городская бибилиотека им. Ф. Т. Селянина."
-			),
-			// Не продлено
-			'rejected' => array(
-				'subject' => 'Книга НЕ продлена',
-				'text'    => "<p>Здравствуйте, $name. </p><br><p>К сожалению, не удалось продлить книгу <i>{$data['book']}</i>. </p><p>Вы можете узнать причину и продлить книгу, позвонив по номеру 6-02-99</p><br> -- \n<br> С уважением, качканарская городская бибилиотека им. Ф. Т. Селянина."
-		));
-		
-		$statusAsText = ( $status? 'accepted' :'rejected' );
-
-		return mail($email, $Messages[$statusAsText]['subject'], $Messages[$statusAsText]['text'], $headers);
-	}
-
-	// Добавить новую заявку
-	public function addRequest($name, $email, $book) {
-		$name   = htmlspecialchars($name);
-		$email  = htmlspecialchars($email);
-		$book   = htmlspecialchars($book);
+		$ip = $_SERVER['REMOTE_ADDR'];
 
 		if(!empty($name) && !empty($email) && !empty($book)) {
-			$this->_db->setQuery("INSERT INTO `booksextension` (`name`, `email`, `book`) VALUES ('$name',  '$email',  '$book');");
+			$this->_db->setQuery("INSERT INTO `#__{$this->_db_tablename}` (`ip`, `name`, `abonement`, `email`, `book`) VALUES ('$ip', '$name', '$abonement', '$email',  '$book');");
 			return $this->_db->execute();
 		} else throw new Book_extension_exception('Не все поля были заполнены! Повторите снова.');
-	}
-
-	/**
-	 * @desc Одобрить заявку (продлить книгу)
-	 * 
-	 * @param <int> $requestID Уникальный номер запроса в базе данных
-	 * 
-	 * @return <boolean> Статус одобрения (удалось или нет)
-	 */
-	public function acceptRequest($requestID) {
-		$requestID = intval($requestID);
-
-		if ( empty($requestID) ) return false;
-
-		$this->_db->setQuery("UPDATE `booksextension` SET `processed` = '1' WHERE `id` = '$requestID';");
-		return $this->_db->execute();
-
-	}
-
-	/**
-	 * @desc Одобрить заявку (продлить книгу)
-	 * 
-	 * @param <int> $requestID Уникальный номер запроса в базе данных
-	 * 
-	 * @return <boolean> Статус одобрения (удалось или нет)
-	 */
-	public function discardRequest($requestID) {
-		$requestID = intval($requestID);
-
-		if ( empty($requestID) ) return false;
-
-		$this->_db->setQuery("UPDATE `booksextension` SET `processed` = '1' WHERE `id` = '$requestID';");
-		return $this->_db->execute();
-	}
-
-	/**
-	 * @desc Получить запрос на продление (один) из базы данных
-	 * 
-	 * @param <int> $requestID Уникальный номер запроса в базе данных
-	 * 
-	 * @return <array/boolean> Поля с данными запроса/false в случае неудачи
-	 */
-	public function getRequest($requestID) {
-		if ( empty($requestID) ) return false;
-
-		$this->_db->setQuery("SELECT * FROM `booksextension`  WHERE `id` = '$requestID' LIMIT 1;");
-		$requestInfo = $this->_db->loadAssoc();
-		
-
-		if ( $requestInfo ) {
-			return $requestInfo;
-		} else return false;
-	}
-
-	/**
-	 * @desc Получить запрос на продление (один) из базы данных
-	 * 
-	 * @param <int> $limit Ограничение на количество выводимых записей
-	 * @param <boolean> $all Если true, то выводит все, даже обработанные заявки
-	 * 
-	 * @return <array/boolean> Поля с данными запроса/false в случае неудачи
-	 */
-	public function getRequests($limit = 10, $all = false) {
-		if (! $this-> isEditor ) return false;
-
-		$this->_db->setQuery("SELECT * FROM `booksextension` ".(!$all? "WHERE `processed` = '0'": '').($limit? " LIMIT $limit": '').';');
-		$response = $this->_db->loadAssocList();
-		
-		if ( !empty($response) ) return $response;
-	}
-
-	public function test($value='') {
-		
 	}
 }
 
 
 class Book_extension_exception extends Exception { }
+
+
+/**
+ * Проверка валидности электропочты
+ * @param  string  $email Электропочта
+ * @return boolean        Результат проверки
+ */
+function validateEmail($email) {
+  return !(preg_match('/^([\w\-\.])+@([\w\-\.])+\.([a-z0-9])+$/i', $email) == 0);
+}
